1d09413b8551f315725ebfc2e53df3106e00d8f4
Isha Mistry <ishamistry371@gmail.com>
2026-05-24 10:41:47 +0530

    added secret vc system

--- mod.py
+++ mod.py
@@ -1,9 +1,11 @@
-import discord# bot  calling bot dont not give permission
+import discord # bot  calling bot dont not give permission
+from discord import app_commands
+import random
 import asyncio#
 import os#(operating system) it will let python interact with my computer
-import yt_dlp# search it seacre from yt
-import re# this command used for lead the built-in regalar expression(built-in means types into serveal categorise based on the nature of the data)
-from discord.ui import view,Button#button working on it
+import yt_dlp # search it seacre from yt
+import re # this command used for lead the built-in regalar expression(built-in means types into serveal categorise based on the nature of the data)
+from discord.ui import View,Button#nbutton working on it
 from dotenv import load_dotenv #env file se information inkalta tha like token
 from discord.ext import commands # this a use for calling command (ext = extension /tools coomand = command system main dicord libary)
 def create_welcome_embed(member):#member when ever they come they got hello from the bot
@@ -16,24 +18,200 @@ intents = discord.Intents.default()# it is use for what information i want to gi
 intents.message_content = True# bot can read message
 intents.members= True #bot can detect join or leave
 bot = commands.Bot(command_prefix= "!", intents=intents)
+#gui grafics user interface
+
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+'''class MusicControls(View):
+     def __init__(self,vc):
+          super().__init__(timeout=None)
+          self.vc=vc
+          @discord.ui.button(label="pause",
+                         style=discord.ButtonStyle.grey)
+          async def pause_button(self,interaction:discord.Interaction,button:Button):
+               if self.vc.is_playing():
+                    self.vc.pause()
+                    await interaction.response.send_message("▶️ RESUMED",ephemeral=True)
+               elif self.vc.is_paused():
+                    self.vc.resume()
+                    await interaction.response.send_message("▶️resumed",ephemeral=True)
+
+          @discord.ui.button(label="skip",style = discord.ButtonStyle.blurple)
+          async def skip_button(self,interaction:discord.Interaction,button:Button):
+               await self.vc.stop()
+               await interaction.response.send_message(
+            "⏭️ Skipped",
+            ephemeral=True)
+          @discord.ui.button(label="stop",style = discord.ButtonStyle.red)
+          async def stop_button(self,interaction:discord.Interaction,button:Button):
+               await self.vc.disconnect()
+               await interaction.response.send_message("⏹️ stopped",
+                                                       ephemeral= True)
+          
+
+'''
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+class MusicControls(View):
+
+    def __init__(self, vc):
+        super().__init__(timeout=None)
+        self.vc = vc
+
+    @discord.ui.button(label="Pause", style=discord.ButtonStyle.grey)
+    async def pause_button(self, interaction: discord.Interaction, button: Button):
+
+        if self.vc.is_playing():
+            self.vc.pause()
+            await interaction.response.send_message("⏸ paused", ephemeral=True)
+
+        elif self.vc.is_paused():
+            self.vc.resume()
+            await interaction.response.send_message("▶ resumed", ephemeral=True)
+
+    @discord.ui.button(label="Stop", style=discord.ButtonStyle.red)
+    async def stop_button(self, interaction: discord.Interaction, button: Button):
+
+        await self.vc.disconnect()
+        await interaction.response.send_message("⏹ stopped", ephemeral=True)
+    @discord.ui.button(label="skip",style = discord.ButtonStyle.blurple)
+    async def skip_button(self,interaction:discord.Interaction,button:Button):
+      self.vc.stop()
+      await interaction.response.send_message(
+            "⏭️ Skipped",
+            ephemeral=True)
+
+#############################################################################################
+YDL_OPTIONS = {
+    'format': 'bestaudio/best',
+    'noplaylist': 'True',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }],
+}
+FFMPEG_OPTIONS = {
+    "before_options": (
+        "-reconnect 1 "
+        "-reconnect_streamed 1 "
+        "-reconnect_delay_max 5 "
+        "-nostdin"
+    ),
+    "options": "-vn"
+}
 #____________________________________________________________________________________________
 # this is for music bot to play song but it will save other song as well
 music_queue={}#empty dictionsry which will help store song 
-async def play_next(ctx):#function i make is for playing next song
-      
-      guild_id=ctx.guild.id 
-      vc=ctx.voice_client
-      if guild_id not in music_queue or not  music_queue[guild_id] :
+async def play_next(ctx):
+
+    guild_id = ctx.guild.id
+    vc = ctx.voice_client
+    if guild_id not in music_queue or not music_queue[guild_id]:
+         if autoplay_servers.get(guild_id) == True:
+            search_query = random.choice(["top english songs",
+                                          "lofi music",
+                                          "top hindi songs",
+                                          "anime songs",
+                                          "phonk",
+                                          "pop hits"])
+            with yt_dlp.YoutubeDL(YDL_OPTIONS) as ydl:
+                 info = ydl.extract_info(
+                    f"ytsearch1:{search_query}",
+                    download=False)
+                 video = info['entries'][0]
+                 music_queue[guild_id] = [{
+                    "url": video['url'],
+                    "title": video['title']
+                }]
+         else:
             return
-      song= music_queue[guild_id].pop(0)
-      source= discord.FFmpegPCMAudio(song["url"],**FFMPEG_OPTIONS)
-      def after_play(error):
-            bot.loop.create_task(play_next(ctx))
-      vc.play(source,after=after_play)
-      embed= discord.Embed(title="now playing!",description=song["title"],
-                           color=discord.Color.red())
-      await ctx.send(embed=embed)
-bot.command()
+    if not music_queue[guild_id]:
+        return
+    if repeat_modes.get(guild_id)== True:
+         song = music_queue[guild_id][0]
+    else:
+         song=music_queue[guild_id].pop(0)
+    source = await discord.FFmpegOpusAudio.from_probe(
+        song["url"],
+        **FFMPEG_OPTIONS
+    )
+    def after_play(error):
+        if error:
+            print(error)
+
+        bot.loop.create_task(play_next(ctx))
+
+    if vc.is_playing():
+         return
+    
+    vc.play(source, after=after_play)
+
+    embed = discord.Embed(
+        title="🎶 Now Playing",
+        description=song["title"],
+        color=discord.Color.red()
+    )
+    view = MusicControls(vc)
+    await ctx.send(embed=embed,view=view)
+#------------------------------------------------------------------------------------------
+@bot.command()
+async def kuku(ctx):
+
+    if ctx.author.voice is None:
+        return await ctx.send("Join a VC first 💖")
+
+    channel = ctx.author.voice.channel
+
+    if ctx.voice_client is None:
+
+        vc = await channel.connect()
+
+        vc.autoplay_enabled = False
+
+        embed = discord.Embed(
+            title="🎵 Kuku Joined VC!",
+            description=(
+                "✨ Kuku is here now!\n\n"
+                "🎶 Use `!play <song>` to play music\n"
+                "💖 Ready for cozy vibes!"
+            ),
+            color=discord.Color.red()
+        )
+
+        await ctx.send(embed=embed)
+
+    else:
+        await ctx.send("I'm already in VC 😎")
+#________________________________________________________________________________________
+
+
+#____________________________________________________________________________________________
+#auto play button
+autoplay_servers={}
+@bot.command()
+async def autoplay(ctx,mode=None):
+      
+      guild_id=ctx.guild.id
+      if mode == "on":
+            autoplay_servers[guild_id]= True
+            embed = discord.Embed(
+            title="🔁 Autoplay Enabled",
+            description="Kuku will now keep playing songs automatically 💖",
+            color=discord.Color.green()
+        )
+            await ctx.send(embed=embed)
+      elif mode == "off":
+            autoplay_servers[guild_id]= False
+            embed = discord.Embed(
+            title="⏹️ Autoplay Disabled",
+            description="Kuku stopped automatic music 🌙",
+            color=discord.Color.red()
+        )
+            await ctx.send(embed=embed)
+      else:
+        await ctx.send("Use: `!autoplay on` or `!autoplay off`")
+
 #__________________________________________________________________________________________
 #this is for bot to goo on any chat 
 
@@ -45,19 +223,6 @@ async def setwelcome(ctx,channel:discord.TextChannel):#channel become tragged a 
       await ctx.send(f"welcome channel set to {channel.mention}")#tell us in discord that which channel is saved
 
 #__________________________________________________________________________________________
-YDL_OPTIONS = {
-    'format': 'bestaudio/best',
-    'noplaylist': 'True',
-    'postprocessors': [{
-        'key': 'FFmpegExtractAudio',
-        'preferredcodec': 'mp3',
-        'preferredquality': '192',
-    }],
-}
-FFMPEG_OPTIONS = {
-    'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 
-    'options': '-vn'
-}
 #_________________________________________________________________________________________
 #this id for checking if bot is log or not
 
@@ -74,7 +239,7 @@ async def on_member_join(member):
           return
       channel = bot.get_channel(channel_id)
       if channel:
-            embed=create_welcome_embed(member)
+            embed= create_welcome_embed()
             await channel.send(embed=embed)
 #########################################################################################
 #________________________________________________________________________________________
@@ -82,7 +247,7 @@ async def on_member_join(member):
 
 
 @bot.command()# its a decorator which tell discord to runn a command
-async def play(ctx,*,query):#its 
+async def play(ctx,*,query):#its
       #user not in vc
       if ctx.author.voice is None:
             await ctx.send("join vc first!")
@@ -101,6 +266,7 @@ async def play(ctx,*,query):#its
             await ctx.voice_client.move_to(channel)
       vc= ctx.voice_client
       try:
+
             with yt_dlp.YoutubeDL(YDL_OPTIONS) as ydl:
                   if re.match(r'https?://',query):
                         search_query = query
@@ -114,7 +280,7 @@ async def play(ctx,*,query):#its
                         video_entry=info
                   url = video_entry['url']
                   title = video_entry['title']
-                  source = discord.FFmpegPCMAudio(url, **FFMPEG_OPTIONS)
+                  source = await discord.FFmpegOpusAudio.from_probe(url, **FFMPEG_OPTIONS)
                   #stop current song
                   vc= ctx.voice_client
                   guild_id=ctx.guild.id
@@ -127,13 +293,17 @@ async def play(ctx,*,query):#its
                                        description=title,
                                        color= discord.Color.red())
                   await ctx.send(embed=embed)
-                  if not vc.is_playing():
-                        await play_next(ctx)
+                  if  vc.is_playing():
+                       return
+                  await play_next(ctx)
       except Exception as e:
             await ctx.send(f"error:{e}")
 
 
 #____________________________________________________________________________________________
+
+
+
 #____________________________________________________________________________________________
 # this for skip replay repeat pause stop button
 @bot.command()
@@ -171,6 +341,38 @@ async def testwelcome(ctx):
       member=ctx.author#fake=me as new member
       embed=create_welcome_embed(member)
       await ctx.send(embed=embed)
+#_________________________________________________________________________________________
+
+repeat_modes= {}
+
+
+@bot.command()
+async def stop(ctx):
+     vc= ctx.voice_client
+     if vc is None:
+          await ctx.send("kuku is not in vc")
+          return
+     if vc.is_playing() or vc.is_paused():
+          vc.stop()
+          guild_id = ctx.guild.id
+          if guild_id in music_queue:
+               music_queue[guild_id].clean()
+               await ctx.send("kuku stopped the music")
+     else:
+          await ctx.send("nothing playing")
+@bot.command()
+async def repeat(ctx,mode=None):
+     guild_id= ctx.guild.id
+     if mode == "on":
+          repeat_modes[guild_id]= True
+          await ctx.send("🔁 repeat enabled")
+     elif mode == "off":
+          repeat_modes[guild_id]=False
+          await ctx.send("repeat disable")
+     else:
+          await ctx.send("use:repeat on/off")
+
+          
 #________________________________________________________________________________________
 #this is for member when they leave the server
 @bot.event 
@@ -180,6 +382,48 @@ async def on_member_remove(member):
              await channel.send(f"{member.mention}left this server :<")
 #pil/pillow libary used fir working with images
 # pil is on work we will se it later
+
+
+#hidden vc
+
+@bot.event 
+async def on_ready():
+     await bot.tree.sync()
+     print(f"logged in as {bot.user}")
+
+
+secret_vcs={}
+bot.tree.command(name="secretentry")
+async def secretentry(interaction:discord.Interaction):
+     guild_id= interaction.guild.id
+     if guild_id not in secret_vcs:
+          await interaction.response.send_message("no secret VC configured😔",
+                                                  ephemeral=True)
+          return
+     vc = interaction.guild.get_channel(secret_vcs[guild_id])
+     await asyncio.sleep(2)
+     await interaction.user.move_to(vc)
+
+
+#admins can set it
+@bot.tree.command(name="setsecretvc")
+@app_commands.checks.has_permissions(administrator=True)
+async def setsecretvc(interaction,channel:discord.VoiceChannel):
+     secret_vcs[interaction.guild.id]= channel.id
+     await interaction.response.send_message(f"secret VC SET TO {channel.name}")
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 load_dotenv()
 TOKEN= os.getenv("DISCORD_TOKEN")
